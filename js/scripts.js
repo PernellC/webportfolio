51f451f0aa166f53346c4fdac54c5c94ed84f8d0
PernellC <PernellCelestine@yahoo.com>
2020-01-30 21:44:43 -0500

    test

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
 console.log("Hi there! Welcome to my web portfolio.")
+var text1 = "Thanks for visiting my page hopefully you say \"lets hire this guy!\"";
+var text2 = "Dont forget to visit my about me page!";
