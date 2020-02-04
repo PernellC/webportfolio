5e3eb15162289b1aec521b6c2d0fef8aa5085974
PernellC <PernellCelestine@yahoo.com>
2020-02-04 18:35:13 -0500

    Ran through js linter

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,10 @@
 console.log("Hi there! Welcome to my web portfolio.")
-var text1 = "Thanks for visiting my page hopefully you say \"lets hire this guy!\"";
-var text2 = "Dont forget to visit my about me page!";
 
-function menuToggle() {
-  var x = document.getElementById("myNavtoggle");
+function menuToggle () {
+  var x = document.getElementById("myNavtoggle")
   if (x.className === "navtoggle") {
-    x.className += "responsive";
-} else {
-  x.className = "navtoggle";
-}
+    x.className += "responsive"
+  } else {
+    x.className = "navtoggle"
+  }
 }
