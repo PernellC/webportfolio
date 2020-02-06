fa447151a9efae1db67c651d8c4640570bdee082
PernellC <PernellCelestine@yahoo.com>
2020-02-06 01:32:29 -0500

    made changes to the hero ID

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 console.log("Hi there! Welcome to my web portfolio.")
 
-function menuToggle () {
+function menuToggle () { // eslint-disable-line no-unused-vars
   var x = document.getElementById("myNavtoggle")
   if (x.className === "navtoggle") {
     x.className += " responsive"
